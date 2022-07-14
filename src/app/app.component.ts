4738bb4e1f7b55f5838fd1ef8f71923b86c48b46
siddhartha3110 <itssid31101998@gmail.com>
2022-07-14 22:06:42 +0530

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { WeatherData } from './Models/weather.models';
+import { WeatherService } from './Services/weather.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  constructor(private weatherService:WeatherService){
+
+  }
+  cityName:string='Lucknow';
+  weatherData?:WeatherData;
+
+
+  ngOnInit(): void {
+  this.getWeatherData(this.cityName);
+  this.cityName='';  
+    
+  }
+  
+  onSubmit(){
+    this.getWeatherData(this.cityName);
+    this.cityName='';
+  }
+
+  private getWeatherData(cityName:string){
+    this.weatherService.getWeatherData(cityName)
+    .subscribe({
+      next:(response)=>{
+        this.weatherData=response;
+        console.log(response);
+      }    
+    });
+
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,21 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  weatherApiBaseUrl:'https://community-open-weather-map.p.rapidapi.com/weather',
+  XRapidAPIHostHeaderName:'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue:'community-open-weather-map.p.rapidapi.com',
+  XRapidAPIKeyHeaderName:'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue:'d800852a58mshf6b165c6f2f9c7ep1ae573jsn6a8fe47b77b9'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
